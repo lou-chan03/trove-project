d756e9510717d8aae706ecd78c38963f4eb17ce9
lou-chan03 <115922471+lou-chan03@users.noreply.github.com>
2022-10-18 00:10:25 +0800

    added LIST declarations
    list declarations needed for hashtable implementation

--- trove.h
+++ trove.h
@@ -26,7 +26,22 @@ extern char *trovePath;
 // Min word length from -l flag
 extern int minLength;
 
-typedef struct _list{
+typedef struct _troveInfo{
         char            *pathName;
-        struct _list    *next;
+        struct _troveInfo       *next;
+} TINFO;
+
+// THESE ARE THE DATATYPES AND FUNCTION DECLARATIONS FOR THE LIST
+// TO BE USED FOR THE HASHTABLE
+
+// LIST DATA TYPE
+typedef struct _list{
+        char            *word;
+        struct _list    *nextWord;
 } LIST;
+
+// FUNCTIONS TO BE USED IN LIST FUNCTIONS
+extern  LIST *list_new(void);
+extern  LIST *list_add(LIST*, char*);
+extern  bool list_find(LIST, char*);
+extern  void list_print(LIST*);
